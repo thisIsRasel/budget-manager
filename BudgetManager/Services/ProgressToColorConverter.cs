5064c40016bf23ca391060e60773ddba09cd0c78
Rasel Ahammed <raselcse20@gmail.com>
2025-12-24 19:54:18 +0600

    Progressbar color converter added

---
+++ BudgetManager/Services/ProgressToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace BudgetManager.Services
+{
+    public class ProgressToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double progress)
+            {
+                if (progress >= 1)
+                {
+                    return Colors.Red;
+                }
+                else if (progress >= 0.75)
+                {
+                    return Color.FromArgb("#ffa8a8");
+                }
+            }
+
+            return Color.FromArgb("#00c0ff");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
