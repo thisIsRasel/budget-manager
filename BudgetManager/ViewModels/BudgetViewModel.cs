c8419ee1c1a6cde949fd5fdbf81ff3317f964871
Rasel Ahammed <raselcse20@gmail.com>
2025-12-24 10:55:51 +0600

    budget chart added

--- BudgetManager/ViewModels/BudgetViewModel.cs
+++ BudgetManager/ViewModels/BudgetViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using BudgetManager.Models;
 using BudgetManager.Services;
 using BudgetManager.Views;
+using Microcharts;
+using SkiaSharp;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 
@@ -22,7 +24,55 @@ namespace BudgetManager.ViewModels
                 OnPropertyChanged(nameof(MonthDisplay));
             }
         }
+
+        private decimal _totalBudget;
+        public decimal TotalBudget
+        {
+            get => _totalBudget;
+            set
+            {
+                _totalBudget = value;
+                OnPropertyChanged(nameof(TotalBudget));
+            }
+        }
+
+        private decimal _totalSpent;
+        public decimal TotalSpent
+        {
+            get => _totalSpent;
+            set
+            {
+                _totalSpent = value;
+                OnPropertyChanged(nameof(TotalSpent));
+            }
+        }
+
+        private decimal _remainingAmount;
+        public decimal RemainingAmount
+        {
+            get => _remainingAmount;
+            set
+            {
+                _remainingAmount = value;
+                OnPropertyChanged(nameof(RemainingAmount));
+            }
+        }
+
+        private Chart? _budgetChart;
+        public Chart? BudgetChart
+        {
+            get => _budgetChart;
+            private set
+            {
+                _budgetChart = value;
+                OnPropertyChanged(nameof(BudgetChart));
+                OnPropertyChanged(nameof(IsChartVisible));
+            }
+        }
+
         public string MonthDisplay => CurrentMonth.ToString("MMM yyyy");
+
+        public bool IsChartVisible => BudgetChart is not null;
 
         public ICommand GoToAddBudgetCommand => new Command(async () =>
         {
@@ -61,19 +111,21 @@ namespace BudgetManager.ViewModels
 
         public async void LoadBudgets()
         {
+            var categories = await _sqlite.GetCategoriesAsync();
             var budgets = await _sqlite.GetMonthlyBudgetsAsync(
                 month: _currentMonth.Month,
                 year: _currentMonth.Year);
 
-            var allEntries = await _sqlite.GetMonthlyEntriesAsync(
+            var monthlyEntries = await _sqlite.GetMonthlyEntriesAsync(
                 month: _currentMonth.Month,
                 year: _currentMonth.Year);
 
             decimal totalBudget = 0;
             decimal totalSpent = 0;
-            var categories = await _sqlite.GetCategoriesAsync();
 
             Budgets.Clear();
+            var random = new Random();
+            var entries = new List<ChartEntry>();
             foreach (var budget in budgets)
             {
                 var budgetCategory = categories
@@ -86,9 +138,8 @@ namespace BudgetManager.ViewModels
                     budgetCategory.Id).ToList();
 
                 // Calculate spent amount for this budget's month/year
-                var spentAmount = allEntries
-                    .Where(e => budgetCategoryIds.Contains(e.CategoryId)
-                        && e.Date.Month == budget.Month && e.Date.Year == budget.Year)
+                var spentAmount = monthlyEntries
+                    .Where(e => budgetCategoryIds.Contains(e.CategoryId))
                     .Sum(e => e.Amount);
 
                 Budgets.Add(new BudgetDisplayItem
@@ -101,6 +152,18 @@ namespace BudgetManager.ViewModels
                     SpentAmount = spentAmount
                 });
 
+                if (spentAmount > 0)
+                {
+                    entries.Add(new ChartEntry((float)spentAmount)
+                    {
+                        Label = budgetCategory.Name,
+                        ValueLabel = "৳ " + spentAmount.ToString("F0"),
+                        Color = SKColor.Parse(GetRandomColor(random)),
+                        TextColor = SKColor.Parse("#FFFFFF"),
+                        ValueLabelColor = SKColor.Parse("#FF5722"),
+                    });
+                }
+
                 totalBudget += budget.Amount;
                 totalSpent += spentAmount;
             }
@@ -108,39 +171,20 @@ namespace BudgetManager.ViewModels
             TotalBudget = totalBudget;
             TotalSpent = totalSpent;
             RemainingAmount = totalBudget - totalSpent;
+
+            BudgetChart = entries.Any() ? new PieChart
+            {
+                Entries = entries,
+                LabelTextSize = 30,
+                BackgroundColor = SKColor.Parse("#374046"),
+                LabelMode = LabelMode.LeftAndRight,
+                GraphPosition = GraphPosition.Center,
+            } : null;
         }
 
-        private decimal _totalBudget;
-        public decimal TotalBudget
+        private string GetRandomColor(Random random)
         {
-            get => _totalBudget;
-            set
-            {
-                _totalBudget = value;
-                OnPropertyChanged(nameof(TotalBudget));
-            }
-        }
-
-        private decimal _totalSpent;
-        public decimal TotalSpent
-        {
-            get => _totalSpent;
-            set
-            {
-                _totalSpent = value;
-                OnPropertyChanged(nameof(TotalSpent));
-            }
-        }
-
-        private decimal _remainingAmount;
-        public decimal RemainingAmount
-        {
-            get => _remainingAmount;
-            set
-            {
-                _remainingAmount = value;
-                OnPropertyChanged(nameof(RemainingAmount));
-            }
+            return String.Format("#{0:X6}", random.Next(0x1000000));
         }
 
         private IEnumerable<int> GetBudgetCategoryIds(
