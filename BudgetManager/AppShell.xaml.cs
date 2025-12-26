c0158d1e058612919a60179ae0057ce8f43124c9
Rasel Ahammed <raselcse20@gmail.com>
2025-12-26 23:02:29 +0600

    exit on double back press

--- BudgetManager/AppShell.xaml.cs
+++ BudgetManager/AppShell.xaml.cs
@@ -2,6 +2,8 @@
 {
     public partial class AppShell : Shell
     {
+        private DateTime _lastBackPressed;
+
         public AppShell()
         {
             InitializeComponent();
@@ -16,6 +18,35 @@
             Routing.RegisterRoute(nameof(Views.YearlyBudgetPage), typeof(Views.YearlyBudgetPage));
             Routing.RegisterRoute(nameof(Views.NotesPage), typeof(Views.NotesPage));
             Routing.RegisterRoute(nameof(Views.NoteEntryPage), typeof(Views.NoteEntryPage));
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            // If there is navigation history, go back normally
+            if (Navigation.NavigationStack.Count > 1)
+            {
+                return base.OnBackButtonPressed();
+            }
+
+            var now = DateTime.UtcNow;
+
+            if ((now - _lastBackPressed).TotalSeconds < 2)
+            {
+                // Allow app to close
+                return base.OnBackButtonPressed();
+            }
+
+            _lastBackPressed = now;
+
+            #if ANDROID
+            Android.Widget.Toast.MakeText(
+                Android.App.Application.Context,
+                "Press back again to exit",
+                Android.Widget.ToastLength.Short).Show();
+            #endif
+
+            // Block single back press
+            return true;
         }
     }
 }
